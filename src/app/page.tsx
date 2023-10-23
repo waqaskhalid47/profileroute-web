729f7203a6af9367b27440c355e3aa2c6a24b756
waqas khalid <waqas.khalid@basecampdata.com>
2023-10-24 03:51:37 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex align-middle justify-center">
-      <h1 className="bg-purple-400 py-2 px-2 mt-10 rounded hover:bg-purple-200 cursor-pointer hover:text-purple-500">Profile Route In Development</h1>
+      <h1 className="bg-purple-400 py-2 px-2 mt-10 rounded hover:bg-purple-200 cursor-pointer hover:text-purple-500">Profile Route In Development !!! yahhhh</h1>
     </main>
   );
 }
