bfea2a341ce96eea8e68f683b9e066ef438fc6ae
waqaskhalid48 <waqas.khalid@basecampdata.com>
2023-10-24 22:03:54 +0500

    update

--- src/app/components/header/navbar.tsx
+++ src/app/components/header/navbar.tsx
@@ -17,7 +17,7 @@ function Navbar() {
   ];
 
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900 ">
+    <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-0 border-b-2 border-indigo-100 ">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center">
           <Image
@@ -31,7 +31,7 @@ function Navbar() {
         <button
           onClick={() => setMobileMenu(!mobileMenu)}
           type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 dark:text-white rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
         >
           <span className="sr-only">Open main mobileMenu</span>
           {!mobileMenu ? (
